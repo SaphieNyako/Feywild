bc36e5273320c77c737a19499f0b9415efd14066
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-07-02 12:57:00 +0200

    patchouli and name fix

--- src/main/java/com/feywild/feywild/entity/render/BasePixieRenderer.java
+++ src/main/java/com/feywild/feywild/entity/render/BasePixieRenderer.java
@@ -1,22 +1,32 @@
 package com.feywild.feywild.entity.render;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.particles.BasicParticleType;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+import javax.annotation.Nullable;
 
 public abstract class BasePixieRenderer<T extends LivingEntity & IAnimatable> extends GeoEntityRenderer<T> {
 
     protected BasePixieRenderer(EntityRendererManager renderManager, AnimatedGeoModel<T> modelProvider) {
         super(renderManager, modelProvider);
         this.shadowRadius = 0.2F;
+    }
+
+    @Override
+    public RenderType getRenderType(T animatable, float partialTicks, MatrixStack stack, @Nullable IRenderTypeBuffer renderTypeBuffer, @Nullable IVertexBuilder vertexBuilder, int packedLightIn, ResourceLocation textureLocation) {
+        return RenderType.entityTranslucent(getTextureLocation(animatable));
     }
 
     @Override
