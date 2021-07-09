15875e671729659e94885563f60f51bed55df8c0
Playelectro <nebo_GOD@yahoo.com>
2021-07-09 19:46:02 +0300

    shock wave

--- src/main/java/com/feywild/feywild/entity/goals/DwarvenAttackGoal.java
+++ src/main/java/com/feywild/feywild/entity/goals/DwarvenAttackGoal.java
@@ -7,8 +7,10 @@ import net.minecraft.command.arguments.EntityAnchorArgument;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.item.FallingBlockEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
@@ -20,12 +22,15 @@ public class DwarvenAttackGoal extends Goal {
     protected LivingEntity targetMonster;
     protected boolean enchantMonstersNearby = false;
     protected int count = 0;
+    protected boolean sendShock = false;
     private Vector3d targetPos;
+
+    // Ancient's note : do not touch
+    private int strength = 2;
 
     public DwarvenAttackGoal(DwarfBlacksmithEntity entity) {
         this.entity = entity;
         this.worldLevel = entity.level;
-
     }
 
     @Override
@@ -41,7 +46,7 @@ public class DwarvenAttackGoal extends Goal {
             if (count == 0) {
                 reset();
             } else if (count == 10) {
-                attackTarget();
+                sendShock = attackTarget();
 
             } else if (count == 80) {
                 entity.setState(1);
@@ -49,7 +54,20 @@ public class DwarvenAttackGoal extends Goal {
 
             } else if (count <= 80) {
                 entity.lookAt(EntityAnchorArgument.Type.EYES, targetMonster.position());
+                if(sendShock){
 
+                    switch (count){
+                        case 8:
+                            summonShockWave(1);
+                            break;
+                        case 6:
+                            summonShockWave(3);
+                            break;
+                        case 4:
+                            summonShockWave(2);
+                            break;
+                    }
+                }
             }
         }
     }
@@ -59,25 +77,72 @@ public class DwarvenAttackGoal extends Goal {
         count = 120;
     }
 
-    protected void attackTarget() {
+    protected boolean attackTarget() {
         if(entity.getRandom().nextDouble() > 0.6){
             targetMonster.hurt(DamageSource.GENERIC, 15.0f);
+            return false;
         }else{
             targetMonster.hurt(DamageSource.GENERIC, 20.0f);
-            summonLightning();
+            return true;
         }
     }
 
 
-    private void summonLightning(){
-        worldLevel.setBlock(targetMonster.blockPosition(), ModBlocks.ELECTRIFIED_GROUND.get().defaultBlockState(), 2);
-        targetMonster.hurt(DamageSource.LIGHTNING_BOLT, 20);
+    private void summonShockWave(int i){
+        BlockPos pos = entity.blockPosition().below();
+
+        FallingBlockEntity fallingBlockEntity;
+
+
+
+        if(i == 3){
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.north().west().getX(),pos.north().west().getY(),pos.north().west().getZ(), worldLevel.getBlockState(pos.north().west()));
+            fallingBlockEntity.setDeltaMovement(0, 0.3d,0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.south().west().getX(),pos.south().west().getY(),pos.south().west().getZ(), worldLevel.getBlockState(pos.south().west()));
+            fallingBlockEntity.setDeltaMovement(0,0.3d,0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.south().east().getX(),pos.south().east().getY(),pos.south().east().getZ(), worldLevel.getBlockState(pos.south().east()));
+            fallingBlockEntity.setDeltaMovement(0,0.3d,0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.north().east().getX(),pos.north().east().getY(),pos.north().east().getZ(), worldLevel.getBlockState(pos.east().north()));
+            fallingBlockEntity.setDeltaMovement(0,0.3d,0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+        }else {
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.north(i).getX(), pos.north(i).getY(), pos.north(i).getZ(), worldLevel.getBlockState(pos.north(i)));
+            fallingBlockEntity.setDeltaMovement(0, 0.3d, 0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.west(i).getX(), pos.west(i).getY(), pos.west(i).getZ(), worldLevel.getBlockState(pos.west(i)));
+            fallingBlockEntity.setDeltaMovement(0, 0.3d, 0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.south(i).getX(), pos.south(i).getY(), pos.south(i).getZ(), worldLevel.getBlockState(pos.south(i)));
+            fallingBlockEntity.setDeltaMovement(0, 0.3d, 0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+
+            fallingBlockEntity = new FallingBlockEntity(worldLevel, pos.east(i).getX(), pos.east(i).getY(), pos.east(i).getZ(), worldLevel.getBlockState(pos.east(i)));
+            fallingBlockEntity.setDeltaMovement(0, 0.3d, 0);
+            fallingBlockEntity.setHurtsEntities(true);
+            worldLevel.addFreshEntity(fallingBlockEntity);
+        }
+
     }
 
     protected void reset() {
         enchantMonstersNearby = false;
         entity.setState(0);
         targetMonster = null;
+        sendShock = false;
     }
 
     @Override
