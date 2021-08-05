2123dd85e383657ed017b568a0df765ab7e7f837
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-08-05 19:21:49 +0200

    Recipe catalysts for JEI

--- src/main/java/com/feywild/feywild/util/JEI.java
+++ src/main/java/com/feywild/feywild/util/JEI.java
@@ -1,15 +1,18 @@
 package com.feywild.feywild.util;
 
 import com.feywild.feywild.FeywildMod;
+import com.feywild.feywild.block.ModBlocks;
 import com.feywild.feywild.recipes.AltarRecipe;
 import com.feywild.feywild.recipes.AltarRecipeCategory;
 import com.feywild.feywild.recipes.DwarvenAnvilRecipe;
 import com.feywild.feywild.recipes.DwarvenAnvilRecipeCategory;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.util.ResourceLocation;
@@ -33,6 +36,12 @@ public class JEI implements IModPlugin {
                 new AltarRecipeCategory(registration.getJeiHelpers().getGuiHelper()),
                 new DwarvenAnvilRecipeCategory(registration.getJeiHelpers().getGuiHelper())
         );
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.feyAltar), AltarRecipeCategory.UID);
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.dwarvenAnvil), DwarvenAnvilRecipeCategory.UID);
     }
 
     @Override
