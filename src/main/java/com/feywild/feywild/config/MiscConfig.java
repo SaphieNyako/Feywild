f8584535e9c591d2e327673c31807a6ea83a8993
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-08-13 10:21:52 +0200

    dust fix

--- src/main/java/com/feywild/feywild/config/MiscConfig.java
+++ src/main/java/com/feywild/feywild/config/MiscConfig.java
@@ -7,8 +7,8 @@ public class MiscConfig {
 
     @Config("Whether players should spawn with a Feywild Lexicon")
     public static boolean initial_lexicon = true;
-    
+
     @Config("The duration in ticks for the levitation effect applied by fey dust.")
     @IntRange(min = 1)
-    public static int fey_dust_ticks = 15;
+    public static int fey_dust_ticks = 30;
 }
