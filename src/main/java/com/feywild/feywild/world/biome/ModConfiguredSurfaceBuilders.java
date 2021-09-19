f116e88729b1e3b8e104de0ca78cf2b3c584ca67
Playelectro <nebo_GOD@yahoo.com>
2021-09-19 18:52:02 +0300

    grass fix

--- src/main/java/com/feywild/feywild/world/biome/ModConfiguredSurfaceBuilders.java
+++ src/main/java/com/feywild/feywild/world/biome/ModConfiguredSurfaceBuilders.java
@@ -11,9 +11,9 @@ import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 
 public class ModConfiguredSurfaceBuilders {
 
-    public static final ConfiguredSurfaceBuilder<?> SPRING_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "spring_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.SAND.defaultBlockState());
-    public static final ConfiguredSurfaceBuilder<?> SUMMER_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "summer_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.SAND.defaultBlockState());
-    public static final ConfiguredSurfaceBuilder<?> AUTUMN_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "autumn_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.SAND.defaultBlockState());
+    public static final ConfiguredSurfaceBuilder<?> SPRING_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "spring_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.DIRT.defaultBlockState(), Blocks.SAND.defaultBlockState());
+    public static final ConfiguredSurfaceBuilder<?> SUMMER_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "summer_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.DIRT.defaultBlockState(), Blocks.SAND.defaultBlockState());
+    public static final ConfiguredSurfaceBuilder<?> AUTUMN_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "autumn_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.DIRT.defaultBlockState(), Blocks.SAND.defaultBlockState());
     public static final ConfiguredSurfaceBuilder<?> WINTER_SURFACE = registerConfiguredSurfaceBuilder(new ResourceLocation(FeywildMod.getInstance().modid, "winter_surface"), Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.SNOW_BLOCK.defaultBlockState(), Blocks.SAND.defaultBlockState());
 
     private static ConfiguredSurfaceBuilder<?> registerConfiguredSurfaceBuilder(ResourceLocation surfaceBuilderRecourseLocation, BlockState topBlock, BlockState fillerBlock, BlockState underwaterBlock) {
