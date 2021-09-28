cc1337d800930dafc30f6e5e547a893951c63b3a
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-09-28 14:42:49 +0200

    patchouli and advancement update

--- src/main/java/com/feywild/feywild/data/AdvancementProvider.java
+++ src/main/java/com/feywild/feywild/data/AdvancementProvider.java
@@ -4,14 +4,19 @@ import com.feywild.feywild.block.ModBlocks;
 import com.feywild.feywild.block.ModTrees;
 import com.feywild.feywild.entity.ModEntityTypes;
 import com.feywild.feywild.item.ModItems;
+import com.feywild.feywild.world.dimension.ModDimensions;
 import io.github.noeppi_noeppi.libx.data.provider.AdvancementProviderBase;
 import io.github.noeppi_noeppi.libx.mod.ModX;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.advancements.criterion.ChangeDimensionTrigger;
 import net.minecraft.advancements.criterion.EntityPredicate;
+import net.minecraft.advancements.criterion.ItemPredicate;
 import net.minecraft.advancements.criterion.VillagerTradeTrigger;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.entity.EntityType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -28,7 +33,7 @@ public class AdvancementProvider extends AdvancementProviderBase {
         this.root()
                 .display(ModItems.feywildLexicon)
                 .task(this.items(ModItems.feywildLexicon));
-        
+
         // Don't ask about this
         try {
             ResourceLocation rl = new ResourceLocation(this.mod.modid, this.mod.modid + "/root");
@@ -55,31 +60,31 @@ public class AdvancementProvider extends AdvancementProviderBase {
         } catch (ReflectiveOperationException e) {
             throw new RuntimeException(e);
         }
-        
+
         this.advancement("fey_dust")
                 .display(ModItems.feyDust)
                 .task(this.items(ModItems.feyDust), this.items(ModItems.lesserFeyGem));
-        
+
         this.advancement("dwarf_trade").parent("fey_dust")
                 .display(ModItems.lesserFeyGem)
                 .task(new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfBlacksmith), this.stack(ModItems.lesserFeyGem)));
-        
+
         this.advancement("dwarf_contract").parent("dwarf_trade")
                 .display(ModItems.summoningScrollDwarfBlacksmith)
                 .task(this.items(ModItems.summoningScrollDwarfBlacksmith));
-        
+
         this.advancement("schematics_fey_altar").parent("dwarf_contract")
                 .display(ModItems.schematicsFeyAltar)
                 .task(this.items(ModItems.schematicsFeyAltar));
-        
+
         this.advancement("schematics_gem_transmutation").parent("dwarf_contract")
                 .display(ModItems.schematicsGemTransmutation)
                 .task(this.items(ModItems.schematicsGemTransmutation));
-        
-        this.advancement("fey_altar").parent("schematics_fey_altar")
+
+        this.advancement("fey_altar")
                 .display(ModBlocks.feyAltar)
                 .task(this.items(ModBlocks.feyAltar));
-        
+
         this.advancement("fey_sapling")
                 .display(ModTrees.springTree.getSapling())
                 .task(
@@ -88,7 +93,7 @@ public class AdvancementProvider extends AdvancementProviderBase {
                         this.items(ModTrees.autumnTree.getSapling()),
                         this.items(ModTrees.winterTree.getSapling())
                 );
-        
+
         this.advancement("summon_spring").parent("fey_altar")
                 .display(ModItems.summoningScrollSpringPixie)
                 .task(this.items(ModItems.summoningScrollSpringPixie));
@@ -104,5 +109,25 @@ public class AdvancementProvider extends AdvancementProviderBase {
         this.advancement("summon_winter").parent("fey_altar")
                 .display(ModItems.summoningScrollWinterPixie)
                 .task(this.items(ModItems.summoningScrollWinterPixie));
+
+        this.advancement("traveling_stone")
+                .display(ModItems.inactiveMarketRuneStone)
+                .task(this.items(ModItems.inactiveMarketRuneStone), this.items(ModItems.marketRuneStone));
+
+        this.advancement("dwarven_market").parent("traveling_stone")
+                .display(ModItems.marketRuneStone)
+                .task(new ChangeDimensionTrigger.Instance(this.entity(EntityType.PLAYER), World.OVERWORLD, ModDimensions.MARKET_PLACE_DIMENSION));
+
+        this.advancement("dwarf_trades").parent("dwarven_market")
+                .display(ModItems.marketRuneStone)
+                .task(
+                        new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfBaker), ItemPredicate.ANY),
+                        new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfShepherd), ItemPredicate.ANY),
+                        new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfArtificer), ItemPredicate.ANY),
+                        new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfToolsmith), ItemPredicate.ANY),
+                        new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfMiner), ItemPredicate.ANY),
+                        new VillagerTradeTrigger.Instance(EntityPredicate.AndPredicate.ANY, this.entity(ModEntityTypes.dwarfDragonHunter), ItemPredicate.ANY)
+                );
+
     }
 }
