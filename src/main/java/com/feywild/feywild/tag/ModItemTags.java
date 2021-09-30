34d1e58418a4f960b65a596077a6c7b3e1141676
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-09-30 14:13:58 +0200

    more compat

--- src/main/java/com/feywild/feywild/tag/ModItemTags.java
+++ src/main/java/com/feywild/feywild/tag/ModItemTags.java
@@ -10,4 +10,8 @@ public class ModItemTags {
 
     public static final ITag.INamedTag<Item> SCHEMATICS = ItemTags.bind(new ResourceLocation(FeywildMod.getInstance().modid, "schematics").toString());
     public static final ITag.INamedTag<Item> FEY_LOGS = ItemTags.bind(new ResourceLocation(FeywildMod.getInstance().modid, "fey_logs").toString());
+    public static final ITag.INamedTag<Item> DEADLY_BOOKS = ItemTags.bind(new ResourceLocation(FeywildMod.getInstance().modid, "deadly_books").toString());
+    public static final ITag.INamedTag<Item> ELEMENTAL_BOOKS = ItemTags.bind(new ResourceLocation(FeywildMod.getInstance().modid, "elemental_books").toString());
+    public static final ITag.INamedTag<Item> SEASONAL_BOOKS = ItemTags.bind(new ResourceLocation(FeywildMod.getInstance().modid, "seasonal_books").toString());
+    public static final ITag.INamedTag<Item> YGGDRASIL_BOOKS = ItemTags.bind(new ResourceLocation(FeywildMod.getInstance().modid, "yggdrasil_books").toString());
 }
