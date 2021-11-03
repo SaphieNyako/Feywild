8065b73b42090ddd5b1268108d98c7090155bfa8
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2021-11-03 19:02:01 +0100

    this works

--- src/main/java/com/feywild/feywild/container/DwarvenAnvilContainerMenu.java
+++ src/main/java/com/feywild/feywild/container/DwarvenAnvilContainerMenu.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.items.SlotItemHandler;
 
 import javax.annotation.Nullable;
 
@@ -18,11 +19,11 @@ public class DwarvenAnvilContainerMenu extends BlockEntityMenu<DwarvenAnvil> {
         super(type, windowId, level, pos, playerContainer, player, 7, 8);
         this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 0, 30, 56)); //this will hold the feydust
         this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 1, 30, 8)); // this will hold the scheme
-        this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 2, 82, 15)); // item 1
-        this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 3, 55, 29)); // item 2
-        this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 4, 112, 29)); // item 3
-        this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 5, 70, 50)); // item 4
-        this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 6, 96, 50)); // item 5
+        this.addSlot(new SlotItemHandler(this.blockEntity.getInventory().getUnrestricted(), 2, 82, 15)); // item 1
+        this.addSlot(new SlotItemHandler(this.blockEntity.getInventory().getUnrestricted(), 3, 55, 29)); // item 2
+        this.addSlot(new SlotItemHandler(this.blockEntity.getInventory().getUnrestricted(), 4, 112, 29)); // item 3
+        this.addSlot(new SlotItemHandler(this.blockEntity.getInventory().getUnrestricted(), 5, 70, 50)); // item 4
+        this.addSlot(new SlotItemHandler(this.blockEntity.getInventory().getUnrestricted(), 6, 96, 50)); // item 5
         this.addSlot(new BaseSlot(this.blockEntity.getInventory(), 7, 149, 56)); // Output
         this.layoutPlayerInventorySlots(8, 84);
         this.addDataSlot(Util.trackHigherOrderBits(this.blockEntity::getMana, this.blockEntity::setMana));
