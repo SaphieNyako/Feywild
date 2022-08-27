e56901cab2d4807edcdf5d0ab3db39e8b64f8c48
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-08-27 18:00:29 +0200

    Remove the legacy worldgen sources (everything should be ported over to use SandBox)

--- src/worldgen_temp/world/feature/structure/structures/BeekeepStructure.java
+++
@@ -1,9 +0,0 @@
-package com.feywild.feywild.world.feature.structure.structures;
-
-public class BeekeepStructure extends BaseStructure {
-
-    public BeekeepStructure() {
-        super("beekeep");
-    }
-
-}
