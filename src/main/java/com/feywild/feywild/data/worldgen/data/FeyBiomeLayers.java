189743d3c1b1b85f1fe1c7e00cd2a7a452b4f364
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-09-01 21:02:26 +0200

    Make all seasonal biomes appear equally often

--- src/main/java/com/feywild/feywild/data/worldgen/data/FeyBiomeLayers.java
+++ src/main/java/com/feywild/feywild/data/worldgen/data/FeyBiomeLayers.java
@@ -11,10 +11,10 @@ public class FeyBiomeLayers extends BiomeLayerData {
     
     public final Holder<BiomeLayer> layer = this.layer(FeywildDimensions.FEYWILD_LAYER)
             .fullRange()
-            .biome(this.biomes.springBiome).temperature(-0.5f, 0.5f).humidity(-1, 0).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
-            .biome(this.biomes.summerBiome).temperature(0.5f, 1).humidity(-1, 1).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
-            .biome(this.biomes.autumnBiome).temperature(-0.5f, 0.5f).humidity(0, 1).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
-            .biome(this.biomes.winterBiome).temperature(-1, -0.5f).humidity(-1, 1).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
+            .biome(this.biomes.springBiome).temperature(0, 1).humidity(0, 1).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
+            .biome(this.biomes.summerBiome).temperature(0, 1).humidity(-1, 0).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
+            .biome(this.biomes.autumnBiome).temperature(-1, 0).humidity(0, 1).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
+            .biome(this.biomes.winterBiome).temperature(-1, 0).humidity(-1, 0).continentalness(-0.18f, 1).erosion(-1, 1).depth(-1, 1).weirdness(-1, 1).add()
             .biome(this.biomes.feywildOcean).temperature(-1, 1).humidity(-1, 1).continentalness(-1, -0.18f).erosion(-1, 1).depth(-1, 0.5f).weirdness(-1, 1).add()
             .build();
 
--- src/main/java/com/feywild/feywild/data/worldgen/data/FeyNoiseSettings.java
+++ src/main/java/com/feywild/feywild/data/worldgen/data/FeyNoiseSettings.java
@@ -6,18 +6,22 @@ import net.minecraft.world.level.levelgen.*;
 
 public class FeyNoiseSettings extends NoiseData {
 
-    public final Holder<DensityFunction> feywildRidges = this.density(
+    public final Holder<DensityFunction> feywildTemperature = this.density(DensityFunctions.noise(this.holder(Noises.TEMPERATURE), 0.25, 1));
+    public final Holder<DensityFunction> feywildVegetation = this.density(DensityFunctions.noise(this.holder(Noises.VEGETATION), 0.25, 1));
+
+    public final Holder<DensityFunction> feywildContinents = this.density(
             DensityFunctions.add(
-                    new DensityFunctions.HolderHolder(this.holder(NoiseRouterData.RIDGES)),
-                    DensityFunctions.constant(0.1)
+                    new DensityFunctions.HolderHolder(this.holder(NoiseRouterData.CONTINENTS)),
+                    DensityFunctions.constant(0.05)
             )
     );
 
     public final Holder<NoiseGeneratorSettings> feywild = this.generator()
-            .router().ridges(this.feywildRidges)
+            .router().temperature(this.feywildTemperature)
+            .router().vegetation(this.feywildVegetation)
+            .router().continents(this.feywildContinents)
             .build();
-            
-    
+
     public FeyNoiseSettings(Properties properties) {
         super(properties);
     }
