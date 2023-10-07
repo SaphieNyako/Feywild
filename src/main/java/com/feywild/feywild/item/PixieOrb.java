c4a92017b9b278b1e3d37e273143fe0db5461571
SaphieNyako <77398437+SaphieNyako@users.noreply.github.com>
2023-10-07 21:36:13 +0200

    pixieorb use on statue fix

--- src/main/java/com/feywild/feywild/item/PixieOrb.java
+++ src/main/java/com/feywild/feywild/item/PixieOrb.java
@@ -1,19 +1,21 @@
 package com.feywild.feywild.item;
 
+import com.feywild.feywild.block.FeyAltarBlock;
 import com.feywild.feywild.entity.BotaniaPixie;
 import com.feywild.feywild.entity.ModEntities;
 import com.feywild.feywild.quest.player.QuestData;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import org.moddingx.libx.mod.ModX;
 
-import javax.annotation.Nonnull;
 import java.util.Objects;
 
 public class PixieOrb extends TooltipItem {
@@ -22,23 +24,32 @@ public class PixieOrb extends TooltipItem {
         super(mod, properties, Component.translatable("message.feywild.pixie_orb"));
     }
 
-    @Nonnull
     @Override
-    public InteractionResultHolder<ItemStack> use(@Nonnull Level level, @Nonnull Player player, @Nonnull InteractionHand hand) {
+    public InteractionResult useOn(UseOnContext context) {
+        Player player = context.getPlayer();
+        Level level = context.getLevel();
+        InteractionHand hand = player.getUsedItemHand();
+        Block block = level.getBlockState(context.getClickedPos()).getBlock();
+
         if (player instanceof ServerPlayer) {
-            BotaniaPixie pixie = new BotaniaPixie(ModEntities.botaniaPixie, level);
-            pixie.setPos(player.getX(), player.getY(), player.getZ());
-            pixie.setVariant(getVariantPlayer(player));
-            level.addFreshEntity(pixie);
-            player.swing(hand, true);
-            if (!Objects.requireNonNull(player).isCreative()) {
-                player.getItemInHand(hand).shrink(1);
-                player.addItem(new ItemStack(Items.ENDER_EYE));
+            if (!(block instanceof FeyAltarBlock)) {
+
+                BotaniaPixie pixie = new BotaniaPixie(ModEntities.botaniaPixie, level);
+                pixie.setPos(player.getX(), player.getY(), player.getZ());
+                pixie.setVariant(getVariantPlayer(player));
+                level.addFreshEntity(pixie);
+                player.swing(hand, true);
+
+                if (!Objects.requireNonNull(player).isCreative()) {
+                    player.getItemInHand(hand).shrink(1);
+                    player.addItem(new ItemStack(Items.ENDER_EYE));
+                }
             }
-            return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), false);
+            return InteractionResult.sidedSuccess(context.getLevel().isClientSide);
         }
-        return InteractionResultHolder.pass(player.getItemInHand(hand));
+        return InteractionResult.PASS;
     }
+
 
     private BotaniaPixie.BotaniaPixieVariant getVariantPlayer(Player player) {
         QuestData data = QuestData.get((ServerPlayer) player);
