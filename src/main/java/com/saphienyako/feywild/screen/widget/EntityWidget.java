e5c36ff91f2161ecc018f0ac23e7468806ef668c
SaphieNyako <mellesanders@protonmail.com>
2026-02-11 21:34:34 +0100

    entity follow mouse fix

--- src/main/java/com/saphienyako/feywild/screen/widget/EntityWidget.java
+++ src/main/java/com/saphienyako/feywild/screen/widget/EntityWidget.java
@@ -21,15 +21,21 @@ public class EntityWidget extends AbstractWidget {
 
     @Override
     public void renderWidget(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        //TODO does this need changing?
-        double scale = ((this.height) / this.entity.getType().getHeight()) * 1.5;
+        double scale = (this.height / this.entity.getType().getHeight()) * 1.5;
+
+        int centerX = this.getX() + this.width / 2;
+        int centerY = this.getY() + this.height + (int)(scale * 48 / 85);
+
+        float dx = centerX - mouseX;
+        float dy = centerY - mouseY;
+
         InventoryScreen.renderEntityInInventoryFollowsMouse(
                 graphics,
-                this.getX() + (this.width / 2),
-                this.getY() + this.height + (int) (scale * 48 / 85),
+                centerX,
+                centerY,
                 (int) scale,
-                -(mouseX - this.getX() - (this.width / 2f)),
-                -(mouseY - this.getY() - (this.height / 2f)),
+                dx,
+                dy,
                 this.entity
         );
     }
