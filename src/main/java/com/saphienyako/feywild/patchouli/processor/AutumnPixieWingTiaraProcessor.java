287294485b66c5809dd8d966fb2aa38ee6b95d71
SaphieNyako <mellesanders@protonmail.com>
2026-07-08 14:49:17 +0200

    patchouli

---
+++ src/main/java/com/saphienyako/feywild/patchouli/processor/AutumnPixieWingTiaraProcessor.java
@@ -0,0 +1,12 @@
+package com.saphienyako.feywild.patchouli.processor;
+
+import com.saphienyako.feywild.patchouli.processor.base.FeywildProcessor;
+
+public class AutumnPixieWingTiaraProcessor extends FeywildProcessor {
+    String recipeId = "feywild:autumn_pixie_wing_tiara";
+
+    @Override
+    public String getRecipeId() {
+        return recipeId;
+    }
+}
